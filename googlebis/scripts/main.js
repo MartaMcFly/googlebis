c1c6d6c3346cb978e7b4037510722a30129df5cb
MartaMcFly <37927133+MartaMcFly@users.noreply.github.com>
2018-04-05 18:59:18 +0200

    Add files via upload

---
+++ googlebis/scripts/main.js
@@ -0,0 +1,30 @@
+// let central= document.querySelector('section');
+
+// central.onclick = function() {
+//     let maSrc = central.getAttribute('src');
+//     if(maSrc === 'images/firefox-icon.png') {
+//       monImage.setAttribute ('src','images/firefox2.png');
+//     } else {
+//       monImage.setAttribute ('src','images/firefox-icon.png');
+//     }
+// }
+
+// function visibility() {
+//     document.getElementById("central").style.visibility = "visible";
+// } 
+
+
+bouton_intro.onclick = function() {
+	document.getElementById("bouton_intro").style.visibility = "hidden";
+	document.getElementById("central").style.visibility = "visible";
+}
+
+function recherche() {
+  let recherche = bar
+  localStorage.setItem('https://www.google.fr/#q=' + recherche);
+  monTitre.textContent = 'Mozilla est cool, ' + monNom;
+}
+
+bouton1.onclick = function(recherche) {
+	document.getElementByClass("").
+}
